fd497910f0b0223b2403c587ace20fee2af4e3b4
Antoine CROISILLE <Antoine.CROISILLE@arcaconseil.local>
2021-06-07 18:23:23 +0200

    level1 done

---
+++ level1/main.py
@@ -0,0 +1,51 @@
+import json
+from datetime import date
+
+class Car:
+    def __init__(self, id, price_per_day, price_per_km):
+        self.id = id
+        self.price_per_day = price_per_day
+        self.price_per_km = price_per_km
+
+class Rental:
+    def __init__(self, id, car_id, start_date, end_date, distance):
+        self.id = id
+        self.car_id = car_id
+        tmp_date = start_date.split('-')
+        self.start_date = date(int(tmp_date[0]), int(tmp_date[1]), int(tmp_date[2]))
+        tmp_date = end_date.split('-')
+        self.end_date = date(int(tmp_date[0]), int(tmp_date[1]), int(tmp_date[2]))
+        self.distance = distance
+
+class App:
+    def __init__(self, input):
+        self.cars = []
+        for car in input['cars']:
+            self.cars.append(Car(car['id'], car['price_per_day'], car['price_per_km']))
+
+        self.rentals = []
+        for rental in input['rentals']:
+            self.rentals.append(Rental(rental['id'], rental['car_id'], rental['start_date'], rental['end_date'], rental['distance']))
+
+    def get_car(self, car_id):
+        return list(filter(lambda x: (x.id == car_id), self.cars))[0]
+
+    def get_rental_prices(self):
+        results = {'rentals': []}
+        for rental in self.rentals:
+            car = self.get_car(rental.car_id)
+            nb_days = int((rental.end_date - rental.start_date).days)
+            price = (car.price_per_day * nb_days) + (car.price_per_km * rental.distance)
+            results['rentals'].append({'id': rental.id, 'price': price})
+
+        return results
+
+if __name__ == '__main__':
+    # Read input.json
+    input = json.load(open("data/input.json"))
+
+    app = App(input)
+    output = app.get_rental_prices()
+
+    with open('data/output.json', 'w') as output_file:
+        json.dump(output, output_file)

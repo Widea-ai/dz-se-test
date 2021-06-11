2922eb2dbce54ed630fce505b6090e0641ebf312
Antoine CROISILLE <Antoine.CROISILLE@arcaconseil.local>
2021-06-11 09:46:19 +0200

    Comments added in level5

--- level5/main.py
+++ level5/main.py
@@ -2,18 +2,48 @@ import json
 from datetime import date
 
 class Actor:
+    """
+    Class Actor
+    """
     def __init__(self, id, name):
+        """
+        Constructor
+        :param id: int
+        Actor Id
+        :param name: string
+        Actor's name
+        """
         self.id = id
         self.name = name
         self.actions = []
 
     def debit(self, amount, rental_id):
+        """
+        Debit the actor related with a rental
+        :param amount: int
+        Amount of the debit
+        :param rental_id:
+        Rental id
+        """
         self.actions.append(Action('debit', amount, rental_id))
 
     def credit(self, amount, rental_id):
+        """
+        Credit the actor related with a rental
+        :param amount: int
+        Amount of the credit
+        :param rental_id:
+        Rental id
+        :return:
+        """
         self.actions.append(Action('credit', amount, rental_id))
 
     def get_actions(self, rental_id):
+        """
+        Collect actions related with a rental
+        :param rental_id: in
+        Rental id
+        """
         results = []
         for action in list(filter(lambda x: (x.rental_id == rental_id), self.actions)):
             results.append({
@@ -25,18 +55,49 @@ class Actor:
         return results
 
 class Action:
+    """
+    Class Action
+    """
     def __init__(self, type, amount, rental_id):
+        """
+        Constructor
+        :param type: string
+        Action type
+        :param amount: int
+        Action amount
+        :param rental_id: int
+        Rental related with the action
+        """
         self.type = type
         self.amount = amount
         self.rental_id = rental_id
 
 class Car:
+    """
+    Class Car
+    """
     def __init__(self, id, price_per_day, price_per_km):
+        """
+        Constructor
+        :param id: int
+        Car id
+        :param price_per_day: int
+        Car price per day
+        :param price_per_km: int
+        Car price per km
+        """
         self.id = id
         self.price_per_day = price_per_day
         self.price_per_km = price_per_km
 
     def apply_discount(self, nb_days):
+        """
+        Apply a specific discount on the car depending on how many days the driver is going to use it
+        :param nb_days: int
+        Number of days the driver is going to use the car
+        :return:
+        Car price per day with discount
+        """
         if nb_days > 1:
             discount = 0.9
         elif nb_days > 4:
@@ -49,7 +110,23 @@ class Car:
         return self.price_per_day * discount
 
 class Rental:
+    """
+    Class Rental
+    """
     def __init__(self, id, car_id, start_date, end_date, distance):
+        """
+        Constructor
+        :param id: int
+        Rental id
+        :param car_id: int
+        Car id related with rental
+        :param start_date: string
+        Start date of the rental
+        :param end_date: string
+        End date of the rental
+        :param distance: int
+        Disantce of the rental
+        """
         self.id = id
         self.car_id = car_id
         tmp_date = start_date.split('-')
@@ -62,10 +139,18 @@ class Rental:
         self.options = []
 
     def compute_price(self, car):
+        """
+        Calculate price for the rental
+        :param car: Car
+        Car object related with the rental
+        """
         price_per_day = car.apply_discount(self.nb_days)
         self.price = (price_per_day * self.nb_days) + (car.price_per_km * self.distance)
 
     def keep_commission(self):
+        """
+        Calculate commission for the rental
+        """
         if hasattr(self, 'price'):
             self.commission = self.price * 0.3
             self.insurance_fee = self.commission / 2
@@ -75,6 +160,10 @@ class Rental:
             raise Exception('You need to compute the price before the commission !')
 
     def get_bill(self):
+        """
+        Print bill for the rental
+        :return: Dictionnary
+        """
         return {
             'id': self.id,
             'price': self.price,
@@ -86,6 +175,11 @@ class Rental:
         }
 
     def get_options(self):
+        """
+        Get options related with the rental
+        :return: Array
+        Options
+        """
         result = []
         for option in self.options:
             result.append(option.type)
@@ -93,12 +187,29 @@ class Rental:
         return result
 
 class Option:
+    """
+    Class Option
+    """
     def __init__(self, id, type):
+        """
+        Constructor
+        :param id: int
+        Option id
+        :param type: string
+        Option type
+        """
         self.id = id
         self.type = type
 
 class App:
+    """
+    Class App
+    """
     def __init__(self, input):
+        """
+        Initialize objects used for the App
+        :param input: Dictionnary
+        """
         self.drivy = Actor(1, 'drivy')
         self.insurance = Actor(2, 'insurance')
         self.assistance = Actor(3, 'assistance')
@@ -115,16 +226,22 @@ class App:
             self.rentals.append(Rental(rental['id'], rental['car_id'], rental['start_date'],
                                               rental['end_date'], rental['distance']))
 
-        self.options = []
         for option in input['options']:
             self.get_rental(option['rental_id']).options.append(Option(option['id'], option['type']))
 
     def compute_rentals(self):
+        """
+        Calculate rental price for each rentals
+        """
         for rental in self.rentals:
             rental.compute_price(self.get_car(rental.car_id))
             rental.keep_commission()
 
     def generate_rental_bills(self):
+        """
+        Generate rental bills for each rentals
+        :return: Dictionnary
+        """
         results = {'rentals': []}
         for rental in self.rentals:
             results['rentals'].append(rental.get_bill())
@@ -132,6 +249,9 @@ class App:
         return results
 
     def compute_actions(self):
+        """
+        Compute actions for eache rentals
+        """
         for rental in self.rentals:
             self.drivy.credit(rental.drivy_fee, rental.id)
             self.assistance.credit(rental.assistance_fee, rental.id)
@@ -140,6 +260,10 @@ class App:
             self.driver.debit(rental.price, rental.id)
 
     def generate_actions_report(self):
+        """
+        Generate action report for each rentals
+        :return: Dictionnary
+        """
         results = {'rentals': []}
         for rental in self.rentals:
             rental_actions = {'id': rental.id,
@@ -155,6 +279,12 @@ class App:
         return results
 
     def get_car(self, car_id):
+        """
+        Get Car from Car list given an id
+        :param car_id: int
+        Car id
+        :return: Car
+        """
         car = list(filter(lambda x: (x.id == car_id), self.cars))[0]
         if car is None:
             raise Exception('There is no car with this id in car list')
@@ -162,6 +292,12 @@ class App:
         return car
 
     def get_rental(self, rental_id):
+        """
+        Get Rental from rental list given an id
+        :param rental_id: int
+        Rental id
+        :return: Rental
+        """
         rental = list(filter(lambda x: (x.id == rental_id), self.rentals))[0]
         if rental is None:
             raise Exception('There is no rental with this id in rental list')
